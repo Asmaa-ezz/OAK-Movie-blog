b82e8f0b942f5b1cde00f25751ed2bfe394bf262
knanahassouna1 <knanahassouna2@gmail.com>
2018-08-29 13:38:59 +0300

    add sign_in and sign_up

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const router = require('./controllers/index');
+const path = require('path');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'..','public')));
 app.use(router);
 
 module.exports = app;
