3d0e7f503a890af65593757d3c3c909a87aef6a1
asmaa-ezz <you@example.com>
2018-08-29 14:10:49 +0300

    add home handler-bar

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const sign_in = require('./sign-in');
 const sign_up = require('./sign-up');
 
-router.get('/', (req, res) => {
-    res.send("Welcome in Home Page");
-});
+const home = require('./home');
+
+router.get('/', home.get);
 
 router.get('/sign_in',sign_in.get);
 router.get('/sign_up',sign_up.get);
