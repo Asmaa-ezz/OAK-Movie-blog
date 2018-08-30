d6bc6c60183d9d0d388395d5d20d696970b72491
Asmaa Al-Madhoun <38081411+Asmaa-ezz@users.noreply.github.com>
2018-08-30 17:13:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 4000;
-const host = process.env.host || 'localhost';
+const host = process.env.HOST || 'localhost';
 
 app.listen(PORT, () => {
   console.log(`the hostname ${host} on port ${PORT}`);
