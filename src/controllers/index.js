fba96602903c6f05ea84632fd2e14ca81bdf9660
asmaa-ezz <you@example.com>
2018-08-30 10:20:36 +0300

    add error file

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const sign_in = require('./sign-in');
 const sign_up = require('./sign-up');
-
 const home = require('./home');
+const error = require('./error');
 
+// to home page
 router.get('/', home.get);
 
 // to sign in
@@ -14,5 +15,12 @@ router.post('/sign_in', sign_in.post);
 // to sign up
 router.get('/sign_up', sign_up.get);
 router.post('/sign_up', sign_up.post);
+
+
+// to error 404
+router.use(error.client);
+
+// to error 500
+router.use(error.server);
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const app = require('./app');
-const PORT = process.env.PORT || 4000;
+
+const PORT = process.env.PORT || 3000;
 const host = process.env.host || 'localhost';
 
 app.listen(PORT, () => {
-    console.log(`the hostname ${host} on port ${PORT}`);
+  console.log(`the hostname ${host} on port ${PORT}`);
 });
 
 module.exports = app;
